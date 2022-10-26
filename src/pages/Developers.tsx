3b5ac0aca79236cb8dbab144e5c2b65f3f0d2927
Victor Abuda <83292234+Victorabuda23@users.noreply.github.com>
2022-10-26 12:36:32 +0800

    Update Developers.tsx

--- src/pages/Developers.tsx
+++ src/pages/Developers.tsx
@@ -6,8 +6,19 @@ const Developers = () => {
         <h1>Developers</h1>
         <div className='p-5 m-5 grid grid-cols-6 gap-2'>            
                 {/* Add your profile here. You can upload your image to the repo under "images"  directory
-                    and link it here.                
-                */}
+                    and link it here. 
+                    
+                */
+            <div className="w-64 sm:w-64 px-4">
+                        <img src="images/Profile.jpg" alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+                    </div>
+                    <h3 className='font-bold mr-1'>Victor Abudas, </h3>
+                    <p className='font-thin italic'>Front-End Developer</p>
+                </div>
+          }
+          
+          <div className="flex flex-wrap justify-center">
+                   
                 {/* grid item */}
                 <div className="flex flex-wrap justify-center">
                     <div className="w-64 sm:w-64 px-4">
